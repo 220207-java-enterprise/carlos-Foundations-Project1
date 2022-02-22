8e609d8ce3a9f4df14f0ece6a5f1ce08f788d316
one5los <carlos.iniguez17@gmail.com>
2022-02-22 08:27:41 -0800

    Database connected to project/Bao help

---
+++ src/main/java/com/revature/erm/ErmDriver.java
@@ -0,0 +1,11 @@
+package com.revature.erm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ErmDriver {
+    public static void main(String[] args) {
+
+    }
+}
